ac9b347dca5c2cea11418dccedbadcd4db0b761c
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-03-21 20:13:04 +0100

    Add contact page form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='page-footer'>
-      <p>&copy; 2021 <span>Simply Recipes</span> Built with {" "}<a href='https://www.gatsby.com/'>Gatsby</a></p>
+      <p>&copy; {new Date().getFullYear} <span>Simply Recipes</span> Built with {" "}<a href='https://www.gatsbyjs.com/'>Gatsby</a></p>
     </footer>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,33 @@ import Layout from '../components/layout'
 const Contact = () => {
   return (
     <Layout>
-          <h1>contact page</h1>
+          <main className='page'>
+           <section className='contact-page'>
+             <article className='contact-info'>
+             <h3>Want To Get in Touch</h3>
+             <p>The Schola is one of the leading international choirs in the UK and abroad. This is not only because they are a very old choir, but because they are a very special and distinctive choir.</p>
+             <p>This is the second of two concerts we are running for The Schola Society</p>
+             <p>Schola Society and for the charity, which has the aim of improving the lives of children and adults in need.</p>
+             </article>
+             <article>
+               <form className='form contact-form'>
+                 <div className='form-row'>
+                   <label htmlFor='name'>your name</label>
+                   <input type='text' name='name' id='name' />
+                 </div>
+                 <div className='form-row'>
+                   <label htmlFor='email'>your email</label>
+                   <input type='text' name='email' id='email' />
+                 </div>
+                 <div className='form-row'>
+                   <label htmlFor='message'>message</label>
+                   <textarea name='message' id='message'></textarea>
+                 </div>
+                 <button type='submit' className='btn block'>submit</button>
+               </form>
+             </article>
+             </section> 
+          </main>
     </Layout>
 
   )
