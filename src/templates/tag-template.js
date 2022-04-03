e41e8dd2da90db18e22f4b256db6d6009e8c490d
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-03 15:22:34 +0200

    Create page tags automatically

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,38 @@
 import React from "react"
-
-const TagTemplate = () => {
+import { graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
+import Layout from "../components/Layout"
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes
   return (
-    <div>
-      <h2>tag template page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tags}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes} />
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetRecipebyTag($tag: String) {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default TagTemplate
